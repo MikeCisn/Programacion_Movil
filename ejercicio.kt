d88005538866ac678497799fafa731e751345738
Miguel <123128620+MikeCisn@users.noreply.github.com>
2023-10-09 17:22:46 -0600

    Create ejercicio.kt

---
+++ ejercicio.kt
@@ -0,0 +1,2 @@
+//Crear un atributo booleano que nos permita saber si aun seguimos vivos, la variable debe ser unicamente de lectura.
+//Utilizaremos ese valor para elegir si colisionar con un goomba en el ciclo for
