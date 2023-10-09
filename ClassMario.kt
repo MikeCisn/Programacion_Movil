d1287424cb8753ef3590424ceb860c089110a2b3
Miguel <123128620+MikeCisn@users.noreply.github.com>
2023-10-09 17:14:38 -0600

    Create ClassMario.kt

---
+++ ClassMario.kt
@@ -0,0 +1,34 @@
+
+class Mario (var vidas: Int =3){
+    int {
+        println("It's a me! Mario!")
+    }
+    private var states = "small"
+    private var lives =3 
+    
+    private fun die (){
+        lives-- 
+        println("Has perdido una vida!")
+    }
+    public fun collision (collisionObj: String){
+        when (collisionObj){
+            "Goomba"-> die()
+            "Super Mushroom" -> state = "Super Martio"
+            "Fire flower" -> state = "Fire Mario"
+            else -> println("Objeto descopnocido ¡no pasa nada!")
+        }
+    }
+}
+
+fun main() {
+    var mario = Mario()
+    mario.collision(collisionObj:"pipe")
+    mario.collision(collisionObj:"Goomba")
+    println("te quedan ${mario.getLives()}")
+    
+}
+
+    for (i in 1..5){
+        mario.collision("Goomba")
+        println("Te quedan ${Mario.getLives()}")
+    }
