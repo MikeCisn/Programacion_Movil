7a9c6a644bddcdddaac7c2351250b4c7302a53cd
Miguel <123128620+MikeCisn@users.noreply.github.com>
2023-10-09 17:38:25 -0600

    Update ejercicio.kt

--- ejercicio.kt
+++ ejercicio.kt
@@ -1,2 +1,9 @@
 //Crear un atributo booleano que nos permita saber si aun seguimos vivos, la variable debe ser unicamente de lectura.
 //Utilizaremos ese valor para elegir si colisionar con un goomba en el ciclo for
+val isAlive: Boolean
+get() {
+  return lives>=1
+}
+
+if (Mario.isAlive){
+  mario.collision(collisionObj)
