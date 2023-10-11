f4b99a880417a32853ab386bb63c829d17be9826
Miguel <123128620+MikeCisn@users.noreply.github.com>
2023-10-11 13:42:01 -0600

    Update ClassMario.kt

--- ClassMario.kt
+++ ClassMario.kt
@@ -1,37 +1,43 @@
 import java.util
 import java.kotlin
+package clases
 
-
-class Mario (var vidas: Int =3){
-    int {
+class Mario (var vidas : Int =3){
+    init {
         println("It's a me! Mario!")
     }
-    private var states = "small"
-    private var lives =3 
-    
+    private var state = "small"
+    private var lives =3
+
     private fun die (){
-        lives-- 
+        lives--
         println("Has perdido una vida!")
     }
     public fun collision (collisionObj: String){
         when (collisionObj){
             "Goomba"-> die()
-            "Super Mushroom" -> state = "Super Martio"
+            "Super Mushroom" -> state = "Super Mario"
+
             "Fire flower" -> state = "Fire Mario"
+
             else -> println("Objeto descopnocido ¡no pasa nada!")
+
         }
     }
+    fun getLives(): String {
+        return "$lives vidas"
+
+    }
 }
+__________________________
 
 fun main() {
-    var mario = Mario()
-    mario.collision(collisionObj:"pipe")
-    mario.collision(collisionObj:"Goomba")
-    println("te quedan ${mario.getLives()}")
-    
-}
-
-    for (i in 1..5){
+    var mario = Mario ()
+    //mario.collision( "Pipe")
+    //mario.collision("Goomba")
+    //println("Te quedan ${mario.getLives()}")
+    for (i in 1..5)
         mario.collision("Goomba")
-        println("Te quedan ${Mario.getLives()}")
-    }
+    println("Te quedan ${mario.getLives()}")
+
+}
